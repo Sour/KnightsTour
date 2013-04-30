148d773b8ab9b4ac95481751fb244435c0eca1f4
Wil Gunter <in.wil.i.trust@gmailcom>
2013-04-29 22:01:18 -0700

    1

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -11,7 +11,7 @@ const int N = 8;
 const int N2 = N*N;
 const int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
 const int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-const int board[N][N];
+int board[N][N];
 
 bool isValid(int x, int y){
 	//Checks to see if the coords exist within the chess board.
@@ -21,7 +21,6 @@ bool isValid(int x, int y){
 	return false;
 }
 
-
 bool solve(int x, int y, int index){
 	if(index == N2)
 		return true;
@@ -30,8 +29,14 @@ bool solve(int x, int y, int index){
 	for(int i = 0; i < 8; i++){
 		testX = x + xMove[i];
 		testY = y + yMove[i];
-		if(isValid(testX, testY)
+		if(isValid(testX, testY) == true){
+			board[testX][testY] = index;
+		}
+		if(solve(testX, testY, index + 1) == true)
 			return true;
+		else
+			board[testX][testY] = -1;
+		return false;
 	}
 }
 
@@ -52,6 +57,9 @@ void initBoard(){
 			board[x][y] = -1;
 		}
 	}
+	
+	//sets our start position.
+	board[0][0] = 0;
 }
 
 bool initSolve(){
