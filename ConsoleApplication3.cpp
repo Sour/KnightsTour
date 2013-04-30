a210d3bd6e360b0bf72ddc7dba63d9caa57f34f0
Wil Gunter <in.wil.i.trust@gmailcom>
2013-04-30 00:38:18 -0700

    kinda working bro

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -2,80 +2,92 @@
 //
 
 #include "stdafx.h"
-#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
+const int xMove[8] = { 2, 1, -1, -2, -2, -1,  1,  2 };
+const int yMove[8] = { 1, 2,  2,  1, -1, -2, -2, -1 };
 const int N = 8;
-const int N2 = N*N;
-const int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-const int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+int startX, startY;
 int board[N][N];
 
+void getSize(){
+
+	string temp = "";
+
+	cout << "Enter X: ";
+	getline(cin, temp);
+	startX = std::stoi(temp);
+
+	cout << "Enter Y: ";
+	getline(cin, temp);
+	startY = std::stoi(temp);
+
+}
+
+void printBoard(){
+	for (int x = 0; x < N; x++){
+		for (int y = 0; y < N; y++){
+			printf(" %2d ", board[x][y]);
+		}
+		printf("\n");
+	}
+}
+
 bool isValid(int x, int y){
-	//Checks to see if the coords exist within the chess board.
-	if (board[x][y] == -1 && y >= 0 && y < N && x >= 0 && x < N){
+	if ( x >= 0 && x < N && y >= 0 && y < N && board[x][y] == -1)
 		return true;
+	return false;
+}
+
+int solveBoard(int x, int y, int index){
+	int tempX, tempY;
+	if(index == N*N){
+		return true;
+	}
+
+	for(int i = 0; i < 8; i++){
+		tempX = x + xMove[i];
+		tempY = y + yMove[i];
+
+		if(isValid(tempX, tempY) == true){
+			board[tempX][tempY] = index;
+
+			if(solveBoard(tempX, tempY, index+1) == true){
+				return true;
+			} else {
+				board[tempX][tempY] = -1;
+			}
+		}
 	}
 	return false;
 }
 
-bool solve(int x, int y, int index){
-	if(index == N2)
-		return true;
-
-	int testX, testY;
-	for(int i = 0; i < 8; i++){
-		testX = x + xMove[i];
-		testY = y + yMove[i];
-		if(isValid(testX, testY) == true){
-			board[testX][testY] = index;
-		}
-		if(solve(testX, testY, index + 1) == true)
-			return true;
-		else
-			board[testX][testY] = -1;
-		return false;
-	}
-}
-
-void printComplete(){
-	for (int x = 0; x < N; x++){
-		for (int y = 0; y < N; y++){
-			printf(" %2d ", board[x][y]); //%2d is two digit number
-		}
-		printf("\n"); //\n is a new line.
-	}
-}
-
-void initBoard(){
-
-	//initilizes '-1' to every spot in our array.
+bool initBoard(){
 	for (int x = 0; x < N; x++){
 		for (int y = 0; y < N; y++){
 			board[x][y] = -1;
 		}
 	}
-	
-	//sets our start position.
-	board[0][0] = 0;
-}
+	board[startX][startY] = 0;
 
-bool initSolve(){
-	if(solve(0,0,1)){
+	if(solveBoard(startX, startY, 1) == false){
+		printf("failed");
 		return false;
-	} 
-	printComplete();
+	} else {printBoard();}
 	return true;
 }
 
 
 
-int main(){
 
+
+int main(){
+	
+	getSize();
 	initBoard();
-	initSolve();
 	getchar();
 	return 0;
 }
