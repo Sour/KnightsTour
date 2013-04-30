5e8ad87695c01d0e944f53ac2476105665d5e9ef
Wil Gunter <in.wil.i.trust@gmailcom>
2013-04-29 21:53:00 -0700

    init push

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,74 @@
+// KT
+//
+
+#include "stdafx.h"
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+const int N = 8;
+const int N2 = N*N;
+const int xMove[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+const int yMove[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+const int board[N][N];
+
+bool isValid(int x, int y){
+	//Checks to see if the coords exist within the chess board.
+	if (board[x][y] == -1 && y >= 0 && y < N && x >= 0 && x < N){
+		return true;
+	}
+	return false;
+}
+
+
+bool solve(int x, int y, int index){
+	if(index == N2)
+		return true;
+
+	int testX, testY;
+	for(int i = 0; i < 8; i++){
+		testX = x + xMove[i];
+		testY = y + yMove[i];
+		if(isValid(testX, testY)
+			return true;
+	}
+}
+
+void printComplete(){
+	for (int x = 0; x < N; x++){
+		for (int y = 0; y < N; y++){
+			printf(" %2d ", board[x][y]); //%2d is two digit number
+		}
+		printf("\n"); //\n is a new line.
+	}
+}
+
+void initBoard(){
+
+	//initilizes '-1' to every spot in our array.
+	for (int x = 0; x < N; x++){
+		for (int y = 0; y < N; y++){
+			board[x][y] = -1;
+		}
+	}
+}
+
+bool initSolve(){
+	if(solve(0,0,1)){
+		return false;
+	} 
+	printComplete();
+	return true;
+}
+
+
+
+int main(){
+
+	initBoard();
+	initSolve();
+	getchar();
+	return 0;
+}
+
