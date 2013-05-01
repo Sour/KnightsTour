2c5eba47b793d47f1b9f4f3021c068f68cde4998
Wil Gunter <in.wil.i.trust@gmailcom>
2013-05-01 16:22:43 -0700

    way broken

--- Warnsdorff's Rule/Warnsdorff's Rule/Warnsdorff's Rule.cpp
+++ Warnsdorff's Rule/Warnsdorff's Rule/Warnsdorff's Rule.cpp
@@ -50,11 +50,43 @@ void printBoard(int i){
 	printf("\n");
 }
 
+bool getLowestIndex(int x, int y,int low){
+	if(board[x][y][1] < low && board[x][y][1] != low) 
+		return true;
+	return false;
+}
+
+bool solveBoard(int x, int y, int index){
+	
+	int tempX, tempY, lowIndex;
+	int low = 9;
+
+	if(index == N*N)
+		return true;
+	
+	for(int i = 0; i < 8; i++){
+
+		tempX = x + xMove[i];
+		tempY = y + yMove[i];
+
+		if(getLowestIndex(tempX, tempY, low) == true);{
+			lowIndex = i;
+	
+		}
+	}
+
+	board[x+xMove[lowIndex]][y+yMove[lowIndex]][0] = index;
+	solveBoard(x+xMove[lowIndex], y+yMove[lowIndex], index);
+
+}
+
+
 int main(){
 
 	initBoard();
 	printBoard(0);
 	printBoard(1);
+	solveBoard(0, 0, 1);
 	getchar();
 	return 0;
 }
