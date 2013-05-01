450a25443eaba36b9f84a88ff3f700ad7bc24b77
Wil Gunter <in.wil.i.trust@gmailcom>
2013-05-01 02:17:46 -0700

    Lots of issues here, just based off other one...
    know issues being that it doesn't keep track of indexs through methods
    so 6 != 6 between counts also main solve method gets warns then
    backtracks -.- lol.

--- Warnsdorff's Rule/Warnsdorff's Rule/Warnsdorff's Rule.cpp
+++ Warnsdorff's Rule/Warnsdorff's Rule/Warnsdorff's Rule.cpp
@@ -1,11 +1,91 @@
-// Warnsdorff's Rule.cpp : Defines the entry point for the console application.
-//
+// Warnsdorff's Rule.cpp
+// - I mean yolo right?
 
 #include "stdafx.h"
 
+const int xMove[8] = { 1,  1, 2,  2, -1, -1, -2, -2 };
+const int yMove[8] = { 2, -2, 1, -1,  2, -2,  1, -1 };
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+const int N = 8;
+int board[N][N];
+
+void initBoard(){
+	for(int x = 0; x < N; x++)
+		for(int y = 0; y < N; y++)
+			board[x][y] = -1;
+}
+
+bool isValid(int x, int y){
+	if( x >= 0 && x < N && y >= 0 && y < N && board[x][y] == -1)
+		return true;
+	return false;
+}
+
+int getSingleCount(int x, int y){
+
+	int tempX, tempY;
+	int count = 0;
+
+	for(int i = 0; i < 8; i++){
+
+		int tempX = x + xMove[i];
+		int tempY = y + yMove[i];
+
+		if(isValid(tempX, tempY) == true)
+			count++;
+	}
+	return count;
+}
+
+int getWarnsLowest(int count[8]){
+	int index, s = count[0];
+	for(int i = 0; i < 8; i++){
+		if(count[i] < s) {
+			s = count[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
+int findValidCount(int x, int y){
+
+	int count[8], tempX, tempY;
+
+	for(int i = 0; i < 8; i++){
+
+		int tempX = x + xMove[i];
+		int tempY = y + yMove[i];
+
+		if(isValid(tempX, tempY) == true)
+			count[i] = getSingleCount(tempX, tempY);
+	}
+	return getWarnsLowest(count);
+}
+
+bool solveBoard(int x, int y, int index){
+	if(index == N*N)
+		return true;
+
+	int yieldIndex = findValidCount(x,y);
+	int tempX = x + xMove[yieldIndex];
+	int tempY = y + yMove[yieldIndex];
+	if(isValid(tempX, tempY) == true){
+		board[tempX][tempY] = index;
+
+		if(solveBoard(tempX, tempY, index+1) == true){
+			return true;
+		} else {
+			board[tempX][tempY] = -1;
+		}
+	}
+}
+
+int main(){
+
+	initBoard();
+	solveBoard(0,0,1);
+	getchar();
 	return 0;
 }
 
